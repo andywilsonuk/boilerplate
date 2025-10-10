19fe88708313b8229624da39098ce90a040a7906
Andy Wilson <code@andywilson.me.uk>
2025-10-10 13:58:28 +0000

    Add error handling for undefined root element in app initialization

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { Ui } from './ui/ui'
 import 'preact/debug'
 
 const rootElement = document.getElementsByTagName('body')[0]
+if (rootElement === undefined) { throw new Error('Root not defined') }
 const appElement: Element | null = rootElement.firstElementChild
 if (appElement === null) { throw new Error('Missing body child') }
 render(<Ui />, createRootFragment(rootElement, appElement))
